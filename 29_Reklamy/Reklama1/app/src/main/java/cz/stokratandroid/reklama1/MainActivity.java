a8b535a2d4bd997bc1d2b65c45b28460c6c29d6a
100xAndroid <128327670+100xAndroid@users.noreply.github.com>
2026-04-27 02:12:23 +0200

    Prvni verze zdrojovych souboru

---
+++ 29_Reklamy/Reklama1/app/src/main/java/cz/stokratandroid/reklama1/MainActivity.java
@@ -0,0 +1,33 @@
+package cz.stokratandroid.reklama1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // inicializace reklamy
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+            }
+        });
+
+        // nacteni reklamy
+        AdView adView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        adView.loadAd(adRequest);
+
+    }
+}
